079ce80d2e7aadd39a3f339e123aa803032ce0bf
tysker <joergoertel@proton.me>
2024-04-19 12:01:29 +0200

    styled PageNotFound page

--- client/src/pages/PageNotFound.tsx
+++ client/src/pages/PageNotFound.tsx
@@ -1,7 +1,12 @@
+import {useNavigate} from "react-router-dom";
+
 export default function PageNotFound() {
-  return (
-    <div>
-      <h1>Page not found 😢</h1>
-    </div>
-  );
+    const navigate = useNavigate();
+    return (
+        <div className="w-[300px] h-[50px] text-center fixed top-[20%] left-[50%] mt-[-25px] ml-[-150px]">
+            <h1 className="font-bold text-3xl">Page not found 😢</h1>
+            <button className="text-blue-600" onClick={() => navigate(-1)}>&larr; Go back</button>
+        </div>
+    );
 }
+
