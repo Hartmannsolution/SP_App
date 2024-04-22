65022494005fefbaae8b0ab6abfd4f59408de710
tysker <joergoertel@proton.me>
2024-04-22 08:15:56 +0200

    added new editor for comment section

--- client/src/features/status/CommentBox.tsx
+++ client/src/features/status/CommentBox.tsx
@@ -2,10 +2,10 @@ import DOMPurify from 'dompurify';
 import parse from 'html-react-parser';
 
 function CommentBox({comment = ""}: { comment: string }) {
-    const sanitizedData = DOMPurify.sanitize(comment); // Sanitize the data for preventing XSS attacks
-    let copy: string[] = sanitizedData.split(""); // Split the string into an array of characters
+    const sanitizedHtmlData = DOMPurify.sanitize(comment); // Sanitize the data for preventing XSS attacks
+    const splitHtmlArrayIntoCharacters: string[] = sanitizedHtmlData.split(''); // Split the string into an array of characters
 
-    const addStyleToListTypes = (array: string[]) => { // Add style to the list types
+    const addStylingToHtmlArray = (array: string[]) => { // Add style to the list types
         for (let i = 0; i < array.length; i++) {
             const first = array[i]; // '<'
             const second = array[i + 1]; // 'o' || 'u'
@@ -16,20 +16,19 @@ function CommentBox({comment = ""}: { comment: string }) {
                 if (second === 'u') array[i + 2] = `${array[i + 2]} class="list-disc list-inside"`
             }
         }
-        return array;
+        return array.join('');
     }
 
-    copy = addStyleToListTypes(copy);
+    const styledHtmlArray = addStylingToHtmlArray(splitHtmlArrayIntoCharacters);
 
     return (
         <>
             {comment &&
                 <div
-                    className="ml-2 border-4 self-end w-[345px] border-green-600 text-blue-700 md:w-1/2 bg-blue-50 md:text-lg md:mx-20">
-                    <div className="p-4 text-sm md:text-2xl">
-                        {parse(copy.join(''))}
-                    </div>
+                    className="ml-2 border-4 self-end w-[345px] border-green-600 text-blue-700 md:w-9/12 p-4 text-sm bg-blue-50 md:text-lg md:mx-20">
+                    {parse(styledHtmlArray)}
                 </div>
+
             }
         </>)
 }
